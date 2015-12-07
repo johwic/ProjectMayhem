4b82d7ff19f88d42c4e6c1d9fb8144b8afa481d5
Johan Wicklund <wicklund@hotmail.com>
2015-12-07 13:13:41 +0100

    Updated to Symfony 2.8

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,7 +22,7 @@ class AppKernel extends Kernel
             new Zeroem\CurlBundle\ZeroemCurlBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
